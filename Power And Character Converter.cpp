5b439afdad6474025c3797e0c79d6a392d509e3e
UncleH25 <oneilljarryd@gmail.com>
2025-05-02 05:27:57 +0200

    Added code that will output the results in a set format using setw, left, and right.

--- Power And Character Converter.cpp
+++ Power And Character Converter.cpp
@@ -20,5 +20,11 @@ int main()
 	//Calculation
 	double result = pow(base, exponent);
 
+	//Output for Calculation
+	cout << "\n--- Power Result ---" << endl;
+	cout << left << setw(15) << "Base: " << setw(10) << right << base << endl;
+	cout << left << setw(15) << "Exponent: " << setw(10) << right << exponent << endl;
+	cout << left << setw(15) << "Result: " << setw(10) << right << result << endl;
+
     return 0;
 }
